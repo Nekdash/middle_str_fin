f91f59fad749a30204198eeb1085f225f4e9d794
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-11-18 12:33:01 +0300

    Update itc_sameChar.cpp

--- itc_sameChar.cpp
+++ itc_sameChar.cpp
@@ -1,14 +1,21 @@
 #include "middle_str.h"
 
 char itc_sameChar(string str){
-    for ( long long i = 0; str[i] != '\0'; i++){
-        if (( str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z')){
-            for ( long long j = 0; str[j] != '\0'; j++){
-                if ( str[j] == str[i]){
-                    return str[j];
-                }
-            }
+    
+    char ch = "";
+    long long i=, c = 1;
+    while(str[i] != '\0'{
+        for(long long j  = i + 1; str[i] != '\0'; j++){
+            if(str[i] == str[j] && str[i] != ' ')
+                c++;
         }
+        if ( c == 2){
+            ch = str[i];
+            return ch;
+        }
+        c =1;i++;
+    
     }
-    return '0';
+    return '1';
+   
 }
